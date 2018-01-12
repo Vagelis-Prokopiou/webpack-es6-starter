21d5aeb57210524c456d279d46ff0280121f7360
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2018-01-12 15:08:41 +0200

    Formatting
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

--- js/functions.js
+++ js/functions.js
@@ -5,8 +5,12 @@ const add = function(num1, num2)
 
 const divide = function(num1, num2)
 {
-    return num1 + num2
+    return num1 / num2
 }
 
-export { add, divide };
+export
+{
+    add
+    , divide
+};
 
--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ import
 } from "./functions";
 
 
-let aaaa = 50;
+let aaaa = 100;
 let a = 4;
 console.log('add()', add(aaaa, a));
 console.log('divide()', divide(aaaa, a));
